53048126b0ca52f7b4994b64afe755df13836563
mktwang <58679939+mktwang@users.noreply.github.com>
2020-04-16 18:07:51 +1000

    Create XOR_decrypt.php

---
+++ XOR_decrypt.php
@@ -0,0 +1,29 @@
+function xor_decrypt($in) {
+    $key = base64_decode('ClVLIh4ASCsCBE8lAxMacFMZV2hdVVotEhhUJQNVAmhSEV4sFxFeaAw%3D');
+    $text = $in;
+    $outText = '';
+
+    // Iterate through each character
+    for($i=0;$i<strlen($text);$i++) {
+    $outText .= $text[$i] ^ $key[$i % strlen($key)];
+    }
+
+    return $outText;
+}
+
+echo xor_decrypt('{"showpassword": "no", "bgcolor": "#ffffff"}');
+
+function xor_decrypt($in) {
+    $key = 'qw8J';
+    $text = $in;
+    $outText = '';
+
+    // Iterate through each character
+    for($i=0;$i<strlen($text);$i++) {
+    $outText .= $text[$i] ^ $key[$i % strlen($key)];
+    }
+
+    return $outText;
+}
+
+echo base64_encode(xor_decrypt('{"showpassword": "yes", "bgcolor": "#ffffff"}'));
